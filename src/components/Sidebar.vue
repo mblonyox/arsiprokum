9fda8b2572846bd62f639e7ed4338f89f24e740f
mblonyox <mblonyox@gmail.com>
2018-07-02 06:48:16 +0700

    Pisahkan sidebar pada komponen terpisah.

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,23 @@
+<v-navigation-drawer
+persistent
+clipped
+v-model="drawer"
+enable-resize-watcher
+fixed
+app
+>
+<v-list>
+  <v-list-tile
+    value="true"
+    v-for="(item, i) in items"
+    :key="i"
+  >
+    <v-list-tile-action>
+      <v-icon v-html="item.icon"></v-icon>
+    </v-list-tile-action>
+    <v-list-tile-content>
+      <v-list-tile-title v-text="item.title"></v-list-tile-title>
+    </v-list-tile-content>
+  </v-list-tile>
+</v-list>
+</v-navigation-drawer>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,11 @@
+<v-toolbar clipped-left app>
+  <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+  <v-toolbar-title v-text="title"></v-toolbar-title>
+  <v-spacer></v-spacer>
+</v-toolbar>
+
+<script>
+  export default {
+    name: 'Toolbar'
+  }
+</script>
