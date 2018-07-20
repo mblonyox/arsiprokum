2396adfe1ac48f7f11d5852d9570f034e144bbbb
Sukirno <mblonyox@yahoo.co.id>
2018-07-20 15:49:11 +0700

    Gunakan parallax pada laman depan.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,24 @@
 <template>
-  <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
+  <v-slide-y-transition mode="out-in">
+    <v-parallax :src="photo">
+      <v-layout
+        align-center
+        column
+        justify-center
+      >
+        <h1>Arsip Biro Hukum</h1>
+        <h4>Aplikasi untuk penanganan arsip pada Biro Hukum Kementerian Keuangan.</h4>
       </v-layout>
-    </v-slide-y-transition>
-  </v-container>
+    </v-parallax>
+  </v-slide-y-transition>
 </template>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<script>
+  import photo1 from '../assets/photos/documents-001.jpg'
+
+  export default {
+    computed: {
+      photo: () => photo1
+    }
+  }
+</script>
